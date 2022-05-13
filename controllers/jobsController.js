10b1d5b54c439aa57bb0ee9f592e58991c9cdb61
nazmul367 <najmulrizvy@gmail.com>
2022-05-13 15:02:53 +0600

    job, job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,17 +1,36 @@
-const createJob = async (req, res) => {
-  res.send('createJob')
-}
+import Job from "../models/Job.js"
+import { StatusCodes } from "http-status-codes"
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
-const deleteJob = async (req, res) => {
-  res.send('deleteJob')
+const createJob = async (req, res) => {
+  const { position, company } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError('Please fill up all fields')
+  }
+
+  req.body.createdBy = req.user.userId
+
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
-  res.send('getAllJobs')
+  const jobs = await Job.find({ createdBy: req.user.userId })
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs: jobs.length,
+    numOfPages: 1
+  })
 }
 
 const updateJob = async (req, res) => {
   res.send('updateJob')
+}
+
+const deleteJob = async (req, res) => {
+  res.send('deleteJob')
 }
 
 const showStats = async (req, res) => {
